83d857c4a9f09d3982f7f2054bcd63070c958e55
Kris <kris_s2@outlook.com>
2026-05-16 19:48:48 -0700

    kata: adding controller implementation.

--- WebApiSseDemo.Api/Controllers/SyncController.cs
+++ WebApiSseDemo.Api/Controllers/SyncController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using WebApiSseDemo.Api.Models;
+using WebApiSseDemo.Api.Services;
 
 namespace WebApiSseDemo.Api.Controllers;
 
-[Route("api/[controller]")]
 [ApiController]
-public class SyncController : ControllerBase
+[Route("api/[controller]")]
+public class SyncController(StreamSyncProgress stream) : ControllerBase
 {
+	[HttpGet("stream")]
+	public ServerSentEventsResult<SyncProgress> Stream(CancellationToken ct)
+		=> TypedResults.ServerSentEvents(stream(ct));
 }
